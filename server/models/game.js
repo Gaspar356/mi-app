d14dd8185ba86b815e5801dc76b4dbbf35f7b03f
Gaspar Andres Martinez <gasparandres2002@gmail.com>
2025-05-26 19:16:40 -0300

    server-model-corrections

--- server/models/game.js
+++ server/models/game.js
@@ -1,16 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
   const Game = sequelize.define('Game', {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: DataTypes.STRING,
     description: DataTypes.TEXT,
     has_offer: DataTypes.BOOLEAN,
-    discount_percentage: DataTypes.DECIMAL(5, 2),
-    base_price: DataTypes.DECIMAL(10, 2),
-    autoIncrement: true
-
-  }, {
-    timestamps: false
+    discount_percentage: DataTypes.FLOAT,
+    base_price: DataTypes.FLOAT,
+    category_id: DataTypes.INTEGER,
   });
+
+  Game.associate = (models) => {
+    Game.belongsTo(models.Category, {
+      foreignKey: 'category_id',
+      as: 'Category',
+    });
+
+    Game.hasMany(models.GameImage, {
+      foreignKey: 'game_id',
+      as: 'images',
+      onDelete: 'CASCADE',
+    });
+  };
 
   return Game;
 };
--- server/models/gameImage.js
+++ server/models/gameImage.js
@@ -1,17 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const GameImage = sequelize.define('GameImage', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, autoIncrement: true
-     },
-    image_url: {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    url: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isUrl: true
-      }
-    }
-  }, {
-    timestamps: false
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   });
+
+  GameImage.associate = (models) => {
+    GameImage.belongsTo(models.Game, {
+      foreignKey: 'game_id',
+      as: 'game',
+      onDelete: 'CASCADE',
+    });
+  };
 
   return GameImage;
 };
